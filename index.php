83383a6ab9ae485e1c6717f883605213a73bec5e
AhdaR-IT <ahdarizki30@gmail.com>
2024-02-05 21:17:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel incidunt dicta fugiat officiis ex, nesciunt amet nam, illo cum quod accusamus fugit est cupiditate repudiandae facilis corporis iste voluptatibus saepe?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
